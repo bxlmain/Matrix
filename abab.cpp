5ed1be86eed4944b8adeff768a2be6503896fc9e
bxlmain <clank627@gmail.com>
2024-02-27 23:55:34 +0300

    correctcorrection

--- abab.cpp
+++ abab.cpp
@@ -1,133 +1,98 @@
 ﻿#include <iostream>
 #include <cstdlib>
 
-class Matrix 
-{
+using namespace std;
+
+class Matrix {
 private:
-    int** data; 
-    unsigned int m; 
-    unsigned int n; 
+    int** data; // Указатель на указатель типа int, который используется для динамического выделения двумерного массива, хранящего элементы матрицы.
+    unsigned int m; //Количество строк матрицы
+    unsigned int n; //Количество столбцов матрицы
 
 public:
-    Matrix(unsigned int rows, unsigned int columns) 
-    {
+    Matrix(unsigned int rows, unsigned int columns) { //Конструктор, принимабющий кол-во строк и столбцов для матрицы,  выделяет память для хранения элементов матрицы и инициализирует их нулями
         m = rows;
         n = columns;
 
-       
         data = new int* [m];
         for (unsigned int i = 0; i < m; i++) {
             data[i] = new int[n];
         }
     }
 
-    ~Matrix() 
-    {
-        
-        for (unsigned int i = 0; i < m; i++) 
-        {
+    ~Matrix() {
+        for (unsigned int i = 0; i < m; i++) { // Деструктор
             delete[] data[i];
         }
         delete[] data;
     }
 
-    
-    void fillRandomValues() 
-    {
-        for (unsigned int i = 0; i < m; i++) 
-        {
-            for (unsigned int j = 0; j < n; j++) 
-            {
+    void fillRandomValues() { // Заполняет матрицу случайными значениями от 0 до 9
+        for (unsigned int i = 0; i < m; i++) {
+            for (unsigned int j = 0; j < n; j++) {
                 data[i][j] = rand() % 10;
             }
         }
     }
 
-    
-    int* operator[](unsigned int index) 
-    {
+    int* operator[](unsigned int index) const { //Позволяет обращаться к строкам матрицы как к массивам
         return data[index];
     }
 
-
-    friend std::ostream& operator<<(std::ostream& os, const Matrix& matrix) 
-
-        for (unsigned int i = 0; i < matrix.m; i++) 
-        {
-            for (unsigned int j = 0; j < matrix.n; j++) 
-            {
+    friend ostream& operator<<(ostream& os, const Matrix& matrix) { //Позволяет красиво печатать матрицу в стандартный вывод или в файл.
+        for (unsigned int i = 0; i < matrix.m; i++) {
+            for (unsigned int j = 0; j < matrix.n; j++) {
                 os << matrix.data[i][j] << ' ';
             }
-            os << std::endl;
+            os << endl;
         }
         return os;
     }
 
-    
-    Matrix& operator+=(const Matrix& other) 
-    {
-        for (unsigned int i = 0; i < m; i++) 
-        {
-            for (unsigned int j = 0; j < n; j++) 
-            {
+    Matrix& operator+=(const Matrix& other) {
+        for (unsigned int i = 0; i < m; i++) {
+            for (unsigned int j = 0; j < n; j++) {
                 data[i][j] += other.data[i][j];
             }
         }
         return *this;
     }
 
-    
-    friend Matrix operator+(const Matrix& matrix1, const Matrix& matrix2) 
-    {
+    friend Matrix operator+(const Matrix& matrix1, const Matrix& matrix2) {
         Matrix result(matrix1.m, matrix1.n);
-        for (unsigned int i = 0; i < matrix1.m; i++) 
-        {
-            for (unsigned int j = 0; j < matrix1.n; j++) 
-            {
+        for (unsigned int i = 0; i < matrix1.m; i++) {
+            for (unsigned int j = 0; j < matrix1.n; j++) {
                 result.data[i][j] = matrix1.data[i][j] + matrix2.data[i][j];
             }
         }
         return result;
     }
 
-    
-    Matrix& operator-=(const Matrix& other) 
-    {
-        for (unsigned int i = 0; i < m; i++) 
-        {
-            for (unsigned int j = 0; j < n; j++) 
-            {
+    Matrix& operator-=(const Matrix& other) {
+        for (unsigned int i = 0; i < m; i++) {
+            for (unsigned int j = 0; j < n; j++) {
                 data[i][j] -= other.data[i][j];
             }
         }
         return *this;
     }
 
-    
-    friend Matrix operator-(const Matrix& matrix1, const Matrix& matrix2) 
-    {
+    friend Matrix operator-(const Matrix& matrix1, const Matrix& matrix2) {
         Matrix result(matrix1.m, matrix1.n);
-        for (unsigned int i = 0; i < matrix1.m; i++) 
-        {
-            for (unsigned int j = 0; j < matrix1.n; j++) 
-            {
+        for (unsigned int i = 0; i < matrix1.m; i++) {
+            for (unsigned int j = 0; j < matrix1.n; j++) {
                 result.data[i][j] = matrix1.data[i][j] - matrix2.data[i][j];
             }
         }
         return result;
     }
 
-    
-    friend Matrix operator*(const Matrix& matrix1, const Matrix& matrix2) 
-    {
+    friend Matrix operator*(const Matrix& matrix1, const Matrix& matrix2) {
         Matrix result(matrix1.m, matrix2.n);
-        for (unsigned int i = 0; i < matrix1.m; i++) 
-        {
-            for (unsigned int j = 0; j < matrix2.n; j++) 
-            {
+        for (unsigned int i = 0; i < result.m; i++) {
+            for (unsigned int j = 0; j < result.n; j++) {
                 result.data[i][j] = 0;
-                for (unsigned int k = 0; k < matrix1.n; k++) 
-                {
+                for (unsigned int k = 0; k < matrix1.n; k++) { // Перебираем элементы строки первой матрицы и столбца второй
                     result.data[i][j] += matrix1.data[i][k] * matrix2.data[k][j];
                 }
             }
@@ -135,19 +100,13 @@ public:
         return result;
     }
 
-   
-    friend bool operator==(const Matrix& matrix1, const Matrix& matrix2) 
-    {
-        if (matrix1.m != matrix2.m || matrix1.n != matrix2.n) 
-        {
+    friend bool operator==(const Matrix& matrix1, const Matrix& matrix2) {
+        if (matrix1.m != matrix2.m || matrix1.n != matrix2.n) {
             return false;
         }
-        for (unsigned int i = 0; i < matrix1.m; i++) 
-        {
-            for (unsigned int j = 0; j < matrix1.n; j++) 
-            {
-                if (matrix1.data[i][j] != matrix2.data[i][j]) 
-                {
+        for (unsigned int i = 0; i < matrix1.m; i++) {
+            for (unsigned int j = 0; j < matrix1.n; j++) {
+                if (matrix1.data[i][j] != matrix2.data[i][j]) {
                     return false;
                 }
             }
@@ -155,31 +114,31 @@ public:
         return true;
     }
 
-    
-    friend bool operator!=(const Matrix& matrix1, const Matrix& matrix2) 
-    {
+    friend bool operator!=(const Matrix& matrix1, const Matrix& matrix2) {
         return !(matrix1 == matrix2);
     }
 };
 
-int main() 
-{
+int main() {
+    srand(static_cast<unsigned>(time(nullptr))); // Для генерации разных случайных чисел при каждом запуске
+
     Matrix matrix1(3, 3);
     matrix1.fillRandomValues();
-    std::cout << "Matrix 1:" << std::endl << matrix1;
+    cout << "Matrix 1:" << endl << matrix1;
 
     Matrix matrix2(3, 3);
     matrix2.fillRandomValues();
-    std::cout << "Matrix 2:" << std::endl << matrix2;
+    cout << "Matrix 2:" << endl << matrix2;
 
     Matrix matrix3 = matrix1 + matrix2;
-    std::cout << "Matrix 1 + Matrix 2:" << std::endl << matrix3;
+    cout << "Matrix 1 + Matrix 2:" << endl << matrix3;
 
-    Matrix matrix4 = matrix1 - matrix2;
-    std::cout << "Matrix 1 - Matrix 2:" << std::endl << matrix4;
+    Matrix difference = matrix1 - matrix2;
+    cout << "Difference of Matrix 1 and Matrix 2:" << difference << endl;
 
-    Matrix matrix5 = matrix1 * matrix2;
-    std::cout << "Matrix 1 * Matrix 2:" << std::endl << matrix5;
+    Matrix product = matrix1 * matrix2;
+    cout << "Product of Matrix 1 and Matrix 2:" << product << endl;
+    
 
     return 0;
 }
